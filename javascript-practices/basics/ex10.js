19e1ef191865ae28534025784653b87e6aa9ca0f
pocpoc0202 <pocpoc0202@naver.com>
2021-11-08 12:15:36 +0900

    메시지

--- javascript-practices/basics/ex10.js
+++ javascript-practices/basics/ex10.js
@@ -4,7 +4,75 @@
  * 1. 내장객체(생성자 함수)
  */
 
-console.log("=========1. 내장객체(생성자 함수)============");
+ console.log("=========1. 내장객체(생성자 함수)============");
 
 var a1 = new Array();
 console.log(typeof(a1), a1 instanceof Array, a1.length);
+
+// 배열을 정의할 때 크기 지정은 별 의미가 없다.
+// 동적으로 배열이 늘어난다.
+
+var a2 = new Array(10);
+a2[0] = 0;
+a2[5] = 5;
+
+// 0 undefined 10
+// 배열 요소에 값을 지정하지 않으면 undefined
+console.log(a2[0], a2[2], a2.length);
+
+// 동적으로 배열이 늘어났음
+a2[10] = 11;
+console.log(a2[0], a2[2], a2.length);
+
+// 초기값을 지정할 수 있다.
+var a3 = new Array(0, 2, 4, 6, 8);
+console.log(a3.length);
+
+// 2. 리터럴(선생님 추천)
+console.log("=========2. 리터럴============");
+var a4 = [function() {
+    console.log("Hello");
+}, 20, "JavaScrpit", true, {
+    name: "마이콜",
+    email: "michol@gmail.com"
+}];
+
+console.log(a4.length);
+for(var i=0; i<a4.length; i++){
+    console.log(a4[i]);
+}
+
+a4[0]();
+
+// Array VS Object
+console.log("=========Array VS Object============");
+a5 = []; // 얜 자동으로 늘려줌
+a5[0] = 0;
+a5["1"] = 1; // 이런식으로 넣어도 index 1번 값으로 들어간다.
+a5["name"] = "둘리";
+console.log(a5.length, a5[0], a5[1], a5["1"], a5["name"]);
+
+for(prop in a5){
+    console.log(prop);
+}
+
+o5 = {}; // 얜 자동으로 안늘려줌!
+o5.length = "길이!";
+o5[0] = 0;
+o5["1"] = 1; // 이런식으로 넣어도 index 1번 값으로 들어간다.
+o5["name"] = "둘리";
+console.log(o5.length, o5["0"], o5[1], o5["1"], o5["name"]);
+for(prop in o5){
+    console.log(prop);
+}
+
+// 유사배열
+console.log("==========================================");
+a5.forEach(function(e, index) {
+    consolel.log(index + " : " + e);
+});
+
+// call / apply => this를 바꿔서 출력할 수 있음
+Array.prototype.forEach.call(o5, function(e, index){
+    consolel.log(index + " : " + e);
+});
---
+++ javascript-practices/basics/ex12.js
@@ -0,0 +1,45 @@
+/**
+ * 
+ * Array 확장(prototype 기반의 확장) : List 메소드 추가
+ * 
+ */
+
+// 두 개의 리스트 메소드 추가
+Array.prototype.insert = function(index, value){
+    if(value instanceof Array){ // 어레이면 스플라이스 후 삽입
+        
+        // 방법 1
+        // for(var i = 0; i < value.length; i++){
+        //     this.splice(index, 0, value[i]);
+        //     index++;
+        // }
+        
+        // 방법 2
+        // 콜백 안에서의 this를 주의해야 한다.
+        // 범위가 바뀌는 것을 "클로저"`라 한다.
+        var _this = this;
+        value.forEach(function(e){ // value를 기준으로 반복
+            _this.splice(index++, 0, e);
+        });
+
+    } else {
+        this.splice(index, 0, value); // 어레이가 아니면 그냥 삽입
+    }
+}
+
+Array.prototype.remove = function(index, value){
+    this.splice(index, 1);
+}
+
+// List로 사용하기
+var a = [1,2,4];
+console.log(a);
+
+a.insert(2,3);
+console.log(a);
+
+a.remove(3);
+console.log(a);
+
+a.insert(2, ['a','b','c']); // 기대: [1, 2, 'a', 'b', 'c', 3]
+console.log(a);             // 결과: [1, 2, ['a', 'b', 'c'], 3] => 따라서 insert 함수에 if문 포함시킴
